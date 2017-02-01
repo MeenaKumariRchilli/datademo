22b6bb33d0c8f8a3e411f45dfe57ed6dca956c2c
MeenaKumari <meena@rchilli.com>
2017-02-01 11:40:52 +0530

    this is editing data

--- src/ca/demo/testdata.java
+++ src/ca/demo/testdata.java
@@ -3,6 +3,8 @@ package ca.demo;
 public class testdata {
 	public static void main(String[] args) {
 		System.out.println("hi demo data testing");
+		System.out.println("datademo");
+		System.out.println("demodata");
 	}
 
 }
